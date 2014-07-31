d1b4df47a4e8d11f8db9bffa628ae64df30d2df6
Stefan Daschek <stefan@daschek.net>
2014-07-31 23:50:01 +0200

    Include rbenv version in multiline prompt.
    Only if a version is set via `rbenv local` or `rbenv shell` (`!` before the version string indicates an version set by `rbenv shell`).

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -24,6 +24,18 @@ function _set_project_and_dir_in_project () {
   fi
 }
 
+function _rbenv_version_for_prompt () {
+  if [[ "$RBENV_VERSION" ]]; then
+    echo " (Ruby!$RBENV_VERSION)"
+  else
+    local local_version
+    local_version=$(rbenv local 2>/dev/null)
+    if [[ $? == 0 ]]; then
+      echo " (Ruby $local_version)"
+    fi
+  fi
+}
+
 PROMPT_COMMAND="_set_project_and_dir_in_project $USE_MULTILINE_PROMPT; $PROMPT_COMMAND"
 
 # We use the prompt to also set the terminal window's title. Mac OS Terminal
@@ -43,12 +55,16 @@ else
   fi
 fi
 
+if declare -F rbenv &>/dev/null; then
+  RBENV_PROMPT="\$(_rbenv_version_for_prompt)"
+fi
+
 if [[ "$USE_MULTILINE_PROMPT" ]]; then
   export GIT_PS1_SHOWDIRTYSTATE=1
   export GIT_PS1_SHOWSTASHSTATE=1
   export GIT_PS1_SHOWUNTRACKEDFILES=1
   export GIT_PS1_SHOWUPSTREAM="auto git"
-  PS1="\[$WINDOW_TITLE\]\[$DarkGray\]– \u \[$LightGray\]\w\[$DarkGray\]\$(__git_ps1 \" (%s)\")\n$PROMPT\[$LightMagenta\] \$ \[$NoColor\]"
+  PS1="\[$WINDOW_TITLE\]\[$DarkGray\]– \u \[$LightGray\]\w\[$DarkGray\]\$(__git_ps1 \" (%s)\")$RBENV_PROMPT\n$PROMPT\[$LightMagenta\] \$ \[$NoColor\]"
 else
   PS1="\[$WINDOW_TITLE\]$PROMPT\[$LightMagenta\] $ \[$NoColor\]"
 fi
